798e3ca9264284ef2aa6c18c6c23fb56917945e3
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-12-27 17:57:51 +0330

    fixed #11, create a simple Hook and some comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,53 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import 'phoenix_html';
+import { Socket } from 'phoenix';
+import { LiveSocket } from 'phoenix_live_view';
+import topbar from '../vendor/topbar';
 import Sortable from 'sortablejs';
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+// Start Hooks object
+let Hooks = {};
+
+// Start hooks Functions, this place we put some hooks we defined in elixir side and communicate with backend
+Hooks.SectionDrag = {
+  mounted() {
+    // This is a simple way based on JS Listener
+    this.el.addEventListener('click', (e) => {
+      console.log(e);
+    });
+
+    // This is a way for sending data to client from backend
+    // example `{:noreply, push_event(socket, "points", %{points: new_points})}` from `<div id="chart" phx-hook="Chart">`
+    // example get from server in client side: `this.handleEvent("points", ({points}) => MyChartLib.addPoints(points))`
+    this.handleEvent('createSection', ({ sectionType }) => {
+      console.log(sectionType);
+
+      // send back to the server
+      this.pushEvent('change-section', {});
+    });
+  },
+};
+
+// Configure and introduce functions to Phoenix LiveView
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute('content');
+let liveSocket = new LiveSocket('/live', Socket, {
+  hooks: Hooks,
+  params: { _csrf_token: csrfToken },
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.delayedShow(200))
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' });
+window.addEventListener('phx:page-loading-start', (info) =>
+  topbar.delayedShow(200)
+);
+window.addEventListener('phx:page-loading-stop', (info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
