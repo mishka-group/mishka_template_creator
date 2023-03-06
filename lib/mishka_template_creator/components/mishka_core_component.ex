da2a7af71ae0d73ec5f68429ee0b0309aad00181
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2023-03-06 10:38:45 +0330

    delete unusable slot and attr from push_modal, based on #4 , #44

--- lib/mishka_template_creator/components/mishka_core_component.ex
+++ lib/mishka_template_creator/components/mishka_core_component.ex
@@ -42,13 +42,8 @@ defmodule MishkaTemplateCreatorWeb.MishkaCoreComponent do
   attr :id, :string, required: true
   attr :show, :boolean, default: false
   attr :on_cancel, JS, default: %JS{}
-  attr :on_confirm, JS, default: %JS{}
 
   slot :inner_block, required: true
-  slot :title
-  slot :subtitle
-  slot :confirm
-  slot :cancel
 
   @spec push_modal(map) :: Phoenix.LiveView.Rendered.t()
   def push_modal(assigns) do
@@ -89,37 +84,7 @@ defmodule MishkaTemplateCreatorWeb.MishkaCoreComponent do
                 </button>
               </div>
               <div id={"#{@id}-content"}>
-                <header :if={@title != []}>
-                  <h1 id={"#{@id}-title"} class="text-lg font-semibold leading-8 text-zinc-800">
-                    <%= render_slot(@title) %>
-                  </h1>
-                  <p
-                    :if={@subtitle != []}
-                    id={"#{@id}-description"}
-                    class="mt-2 text-sm leading-6 text-zinc-600"
-                  >
-                    <%= render_slot(@subtitle) %>
-                  </p>
-                </header>
                 <%= render_slot(@inner_block) %>
-                <div :if={@confirm != [] or @cancel != []} class="ml-6 mb-4 flex items-center gap-5">
-                  <.button
-                    :for={confirm <- @confirm}
-                    id={"#{@id}-confirm"}
-                    phx-click={@on_confirm}
-                    phx-disable-with
-                    class="py-2 px-3"
-                  >
-                    <%= render_slot(confirm) %>
-                  </.button>
-                  <.link
-                    :for={cancel <- @cancel}
-                    phx-click={hide_modal(@on_cancel, @id)}
-                    class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                  >
-                    <%= render_slot(cancel) %>
-                  </.link>
-                </div>
               </div>
             </.focus_wrap>
           </div>
