e3c97d5e61df3b941b4403be4221b331e0ac900d
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2023-03-07 21:24:08 +0330

    add Tailwind Filters settings as hard-coded data, based on #44

--- lib/mishka_template_creator/components/blocks/settings.ex
+++ lib/mishka_template_creator/components/blocks/settings.ex
@@ -6462,7 +6462,184 @@ defmodule MishkaTemplateCreator.Components.Blocks.Settings do
           "bg-blend-luminosity"
         ], [:hover, :media_queries]}
      ]},
-    {"filters", "Filters", "Heroicons.scissors", []},
+    {"filters", "Filters", "Heroicons.scissors",
+     [
+       {"blur", "Blur", "Utilities for applying blur filters to an element.",
+        [
+          "blur-none",
+          "blur-sm",
+          "blur",
+          "blur-md",
+          "blur-lg",
+          "blur-xl",
+          "blur-2xl",
+          "blur-3xl"
+        ], [:hover, :media_queries]},
+       {"brightness", "Brightness", "Utilities for applying brightness filters to an element.",
+        [
+          "brightness-0",
+          "brightness-50",
+          "brightness-75",
+          "brightness-90",
+          "brightness-95",
+          "brightness-100",
+          "brightness-105",
+          "brightness-110",
+          "brightness-125",
+          "brightness-150",
+          "brightness-200"
+        ], [:hover, :media_queries]},
+       {"contrast", "Contrast", "Utilities for applying brightness filters to an element.",
+        [
+          "contrast-0",
+          "contrast-50",
+          "contrast-75",
+          "contrast-100",
+          "contrast-125",
+          "contrast-150",
+          "contrast-200"
+        ], [:hover, :media_queries]},
+       {"drop-shadow", "Drop Shadow", "Utilities for applying drop-shadow filters to an element.",
+        [
+          "drop-shadow-sm",
+          "drop-shadow",
+          "drop-shadow-md",
+          "drop-shadow-lg",
+          "drop-shadow-xl",
+          "drop-shadow-2xl",
+          "drop-shadow-none"
+        ], [:hover, :media_queries]},
+       {"grayscale", "Grayscale", "Utilities for applying grayscale filters to an element.",
+        [
+          "grayscale-0",
+          "grayscale"
+        ], [:hover, :media_queries]},
+       {"hue-rotate", "Hue Rotate", "Utilities for applying drop-shadow filters to an element.",
+        [
+          "hue-rotate-0",
+          "hue-rotate-15",
+          "hue-rotate-30",
+          "hue-rotate-60",
+          "hue-rotate-90",
+          "hue-rotate-180"
+        ], [:hover, :media_queries]},
+       {"invert", "Invert", "Utilities for applying invert filters to an element.",
+        [
+          "invert-0",
+          "invert"
+        ], [:hover, :media_queries]},
+       {"saturate", "Saturate", "Utilities for applying saturation filters to an element.",
+        [
+          "saturate-0",
+          "saturate-50",
+          "saturate-100",
+          "saturate-150",
+          "saturate-200"
+        ], [:hover, :media_queries]},
+       {"sepia", "Sepia", "Utilities for applying sepia filters to an element.",
+        [
+          "sepia-0",
+          "sepia"
+        ], [:hover, :media_queries]},
+       {"backdrop-blur", "Backdrop Blur",
+        "Utilities for applying backdrop blur filters to an element.",
+        [
+          "backdrop-blur-none",
+          "backdrop-blur-sm",
+          "backdrop-blur",
+          "backdrop-blur-md",
+          "backdrop-blur-lg",
+          "backdrop-blur-xl",
+          "backdrop-blur-2xl",
+          "backdrop-blur-3xl"
+        ], [:hover, :media_queries]},
+       {"backdrop-brightness", "Backdrop Brightness",
+        "Utilities for applying backdrop brightness filters to an element.",
+        [
+          "backdrop-brightness-0",
+          "backdrop-brightness-50",
+          "backdrop-brightness-75",
+          "backdrop-brightness-90",
+          "backdrop-brightness-95",
+          "backdrop-brightness-100",
+          "backdrop-brightness-105",
+          "backdrop-brightness-110",
+          "backdrop-brightness-125",
+          "backdrop-brightness-150",
+          "backdrop-brightness-200"
+        ], [:hover, :media_queries]},
+       {"backdrop-contrast", "Backdrop Contrast",
+        "Utilities for applying backdrop contrast filters to an element.",
+        [
+          "backdrop-brightness-0",
+          "backdrop-brightness-50",
+          "backdrop-brightness-75",
+          "backdrop-brightness-90",
+          "backdrop-brightness-95",
+          "backdrop-brightness-100",
+          "backdrop-brightness-105",
+          "backdrop-brightness-110",
+          "backdrop-brightness-125",
+          "backdrop-brightness-150",
+          "backdrop-brightness-200"
+        ], [:hover, :media_queries]},
+       {"backdrop-grayscale", "Backdrop Grayscale",
+        "Utilities for applying backdrop grayscale filters to an element.",
+        [
+          "backdrop-grayscale-0",
+          "backdrop-grayscale"
+        ], [:hover, :media_queries]},
+       {"backdrop-hue-rotate", "Backdrop Hue Rotate",
+        "Utilities for applying backdrop hue-rotate filters to an element.",
+        [
+          "backdrop-hue-rotate-0",
+          "backdrop-hue-rotate-15",
+          "backdrop-hue-rotate-30",
+          "backdrop-hue-rotate-60",
+          "backdrop-hue-rotate-90",
+          "backdrop-hue-rotate-180"
+        ], [:hover, :media_queries]},
+       {"backdrop-invert", "Backdrop Invert",
+        "Utilities for applying backdrop invert filters to an element.",
+        [
+          "backdrop-invert-0",
+          "backdrop-invert"
+        ], [:hover, :media_queries]},
+       {"backdrop-opacity", "Backdrop Opacity",
+        "Utilities for applying backdrop opacity filters to an element.",
+        [
+          "backdrop-opacity-0",
+          "backdrop-opacity-5",
+          "backdrop-opacity-10",
+          "backdrop-opacity-20",
+          "backdrop-opacity-25",
+          "backdrop-opacity-30",
+          "backdrop-opacity-40",
+          "backdrop-opacity-50",
+          "backdrop-opacity-60",
+          "backdrop-opacity-70",
+          "backdrop-opacity-75",
+          "backdrop-opacity-80",
+          "backdrop-opacity-90",
+          "backdrop-opacity-95",
+          "backdrop-opacity-100"
+        ], [:hover, :media_queries]},
+       {"backdrop-saturate", "Backdrop Saturate",
+        "Utilities for applying backdrop saturation filters to an element.",
+        [
+          "backdrop-saturate-0",
+          "backdrop-saturate-50",
+          "backdrop-saturate-100",
+          "backdrop-saturate-150",
+          "backdrop-saturate-200"
+        ], [:hover, :media_queries]},
+       {"backdrop-sepia", "Backdrop Sepiae",
+        "Utilities for applying backdrop sepia filters to an element.",
+        [
+          "backdrop-sepia-0",
+          "backdrop-sepia"
+        ], [:hover, :media_queries]}
+     ]},
     {"tables", "Tables", "Heroicons.table_cells", []},
     {"transitions_animation", "Transitions & Animation", "Heroicons.sparkles", []},
     {"transforms", "Transforms", "Heroicons.rectangle_stack", []},
