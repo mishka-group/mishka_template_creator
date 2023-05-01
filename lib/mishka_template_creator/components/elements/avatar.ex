0821a774cadbf9208a8ab4bc210699045ee6ddb2
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2023-05-01 13:14:36 +0330

    add adding and deleteing an avatar, based on #35

--- lib/mishka_template_creator/components/elements/avatar.ex
+++ lib/mishka_template_creator/components/elements/avatar.ex
@@ -73,7 +73,8 @@ defmodule MishkaTemplateCreator.Components.Elements.Avatar do
           MishkaCoreComponent.sorted_list_by_order(@element["order"], @element["children"])
       }>
         <img
-          :if={data["image"] != ""}
+          :if={data["image"] != "" and data["text"] == ""}
+          }
           class={@element["image_class"]}
           src={data["image"]}
           alt={data["alt"]}
@@ -130,16 +131,147 @@ defmodule MishkaTemplateCreator.Components.Elements.Avatar do
           id={"avatar-#{@id}"}
           title="Avatar Settings"
           title_class="my-4 w-full text-center font-bold select-none text-lg"
-          open={false}
         >
           <:before_title_block>
             <Heroicons.plus
               class="w-5 h-5 cursor-pointer"
               phx-click="add"
-              phx-value-type="button"
+              phx-value-type="avatar"
               phx-target={@myself}
             />
           </:before_title_block>
+
+          <div class="w-full flex flex-col gap-3 space-y-4 pt-3">
+            <span
+              :if={length(@element["order"]) == 0}
+              class="mx-auto border border-gray-200 bg-gray-100 font-bold py-2 px-3"
+            >
+              There is no avatar item for this section
+            </span>
+
+            <div
+              :for={
+                {%{id: key, data: data}, index} <-
+                  Enum.with_index(
+                    MishkaCoreComponent.sorted_list_by_order(
+                      @element["order"],
+                      @element["children"]
+                    ),
+                    1
+                  )
+              }
+              class="w-full flex flex-col justify-normal"
+            >
+              <div class="w-full flex flex-row justify-between items-center">
+                <span class="font-bold text-base">
+                  Avatar-<%= index %>
+                </span>
+
+                <div class="flex flex-row justify-end items-center gap-2">
+                  <div
+                    class="flex flex-row justify-center items-start gap-2 cursor-pointer"
+                    phx-click={
+                      JS.toggle(to: "#avatar-common-avatar-#{key}")
+                      |> JS.toggle(to: "#avatar-common-close-#{key}")
+                    }
+                  >
+                    <Heroicons.pencil_square class="w-5 h-5" />
+                    <span class="text-base select-none">
+                      Edit
+                    </span>
+                  </div>
+                  <div
+                    class="flex flex-row justify-center items-start gap-2 cursor-pointer"
+                    phx-click="delete"
+                    phx-value-type="avatar"
+                    phx-value-id={key}
+                    phx-target={@myself}
+                  >
+                    <Heroicons.trash class="w-5 h-5 text-red-600" />
+                    <span class="text-base select-none">Delete</span>
+                  </div>
+                </div>
+              </div>
+
+              <div id={"avatar-common-avatar-#{key}"} class="w-full hidden">
+                <MishkaCoreComponent.custom_simple_form
+                  :let={f}
+                  for={%{}}
+                  as={:avatar_avatar}
+                  phx-change="edit"
+                  phx-target={@myself}
+                  class="flex flex-col w-full justify-start gap-2"
+                >
+                  <div class="flex flex-col gap-2 w-full my-5">
+                    <span class="font-bold text-sm">Title:</span>
+                    <%= text_input(f, :title,
+                      class:
+                        "w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500",
+                      placeholder: "Change avatar name",
+                      value: data["title"],
+                      id: "input-title-#{key}"
+                    ) %>
+                  </div>
+                  <div class="flex flex-col gap-2 w-full">
+                    <span class="font-bold text-sm">Hyperlink:</span>
+                    <%= text_input(f, :hyperlink,
+                      class:
+                        "w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500",
+                      placeholder: "Change avatar hyperlink",
+                      value: data["hyperlink"],
+                      id: "input-hyperlink-#{key}"
+                    ) %>
+                  </div>
+
+                  <div class="flex flex-row gap-2 w-full my-5">
+                    <div class="flex flex-col gap-2 w-full">
+                      <span class="font-bold text-sm">Target:</span>
+                      <%= select(
+                        f,
+                        :target,
+                        [
+                          None: "none",
+                          "New Window or Tab": "_blank",
+                          "Current Window": "_self",
+                          "Parent Window": "_parent",
+                          "Top Frame": "_top"
+                        ],
+                        selected: data["target"],
+                        class:
+                          "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2",
+                        id: "input-target-#{key}"
+                      ) %>
+                    </div>
+
+                    <div class="flex flex-col gap-2 w-full">
+                      <span class="font-bold text-sm">Nofollow:</span>
+                      <%= select(f, :nofollow, [true, false],
+                        selected: data["nofollow"],
+                        class:
+                          "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2",
+                        id: "input-nofollow-#{key}"
+                      ) %>
+                    </div>
+
+                    <.input field={f[:id]} type="hidden" value={key} id={"input-id-#{key}"} />
+                  </div>
+                </MishkaCoreComponent.custom_simple_form>
+                <div class="grid grid-cols-3 gap-2 w-full my-5 pt-2 items-center">
+                  sss
+                </div>
+              </div>
+              <p
+                id={"avatar-common-close-#{key}"}
+                class="text-blue-400 my-2 w-full text-center hidden"
+                phx-click={
+                  JS.toggle(to: "#avatar-common-avatar-#{key}")
+                  |> JS.toggle(to: "#avatar-common-close-#{key}")
+                }
+              >
+                Close this settings
+              </p>
+            </div>
+          </div>
         </Aside.aside_accordion>
 
         <Aside.aside_accordion
@@ -207,6 +339,24 @@ defmodule MishkaTemplateCreator.Components.Elements.Avatar do
     {:noreply, socket}
   end
 
+  def handle_event("add", %{"type" => "avatar"}, socket) do
+    unique_id = Ecto.UUID.generate()
+    avatars = TailwindSetting.default_element("avatar")
+    first_avatars = avatars["children"][List.first(avatars["order"])]
+
+    updated =
+      socket.assigns.element
+      |> update_in(["children"], fn selected_children ->
+        Map.merge(selected_children, %{"#{unique_id}" => first_avatars})
+      end)
+      |> Map.merge(%{"order" => socket.assigns.element["order"] ++ [unique_id]})
+      |> Map.merge(socket.assigns.selected_form)
+
+    send(self(), {"element", %{"update_parame" => updated}})
+
+    {:noreply, socket}
+  end
+
   def handle_event("validate", %{"public_tag" => %{"tag" => tag}}, socket) do
     submit_status =
       Regex.match?(~r/^[A-Za-z][A-Za-z0-9-]*$/, String.trim(tag)) and
@@ -267,6 +417,20 @@ defmodule MishkaTemplateCreator.Components.Elements.Avatar do
            |> Map.merge(socket.assigns.selected_form)
        }}
     )
+
+    {:noreply, socket}
+  end
+
+  def handle_event("delete", %{"type" => "avatar", "id" => id}, socket) do
+    {_, elements} = pop_in(socket.assigns.element, ["children", id])
+
+    updated =
+      Map.merge(elements, %{
+        "order" => Enum.reject(socket.assigns.element["order"], &(&1 == id))
+      })
+      |> Map.merge(socket.assigns.selected_form)
+
+    send(self(), {"element", %{"update_parame" => updated}})
 
     {:noreply, socket}
   end
